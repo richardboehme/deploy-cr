47d03b165f42717bc4cc0f85cd02e45f8d8a0ffe
Richard Böhme <richard.boehme1999@gmail.com>
2021-06-25 18:27:21 +0200

    Remove #restart! step
    Firstly, not every deployed application needs to be restarted. Also
    restarting is very specific to your specific environment. Just add an
    extra step yourself if you need restarting behaviour.

--- src/deploy-cr/deployment.cr
+++ src/deploy-cr/deployment.cr
@@ -2,11 +2,9 @@ class DeployCR::Deployment < DeployCR::Operation
   property! path : String
   property app_name : String
   property files_to_upload : Array(String)
-  property! service : String
 
   def initialize(
     @app_name = app_name_by_path,
-    @service = app_name,
     @files_to_upload = ["bin/#{app_name}.o", "config/environments/.production.enc", "config/database.yml"]
   ); end
 
@@ -14,9 +12,5 @@ class DeployCR::Deployment < DeployCR::Operation
     # we need to use shell option here because otherwise * would not expand correctly
     # rsync -avmz -e ssh --include=files_to_upload.first --include=files_to_upload.second [...] --exclude="*"
     run_process("rsync -avmz -e ssh --include=\"*/\" #{files_to_upload.map { |file| "--include=\"#{file}\"" }.join(" ")} --exclude=\"*\" . #{user}@#{host}:#{path}", shell: true)
-  end
-
-  def restart!
-    ssh_process("systemctl --user restart #{service}")
   end
 end
